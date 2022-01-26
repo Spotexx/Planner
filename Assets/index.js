def7845252c0523de8c4d58c931c167ea69b55ba
Spotexx <travesimo@gmail.com>
2022-01-26 00:08:55 -0500

    pseudo code for tomorow

--- Assets/index.js
+++ Assets/index.js
@@ -1,1 +1,5 @@
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+//create elements for time blocks for each hour 9-5
+//color code each time block based on past, present, or future grey, red, green
+//add event listener to each time block to add event to local storage
