e98fd4ac0571b0d1f55c04dbab82c6c9227a0476
Spotexx <travesimo@gmail.com>
2022-01-27 17:32:54 -0500

    added time coloring

--- Assets/index.js
+++ Assets/index.js
@@ -1,4 +1,5 @@
-$('#currentDay').text(moment().format('dddd, MMMM Do'));
+let currentTime = moment();
+$('#currentDay').text(currentTime.format('dddd, MMMM Do'));
 
 //stores array of notes to local storage
 $( "form" ).submit(function( event ) {
@@ -12,8 +13,19 @@ $( "form" ).submit(function( event ) {
     localStorage.setItem('array', JSON.stringify(array));
 });
 
-// loads arrays into text fields on load
+// loads arrays into text fields on load then feeds background color depending on current time(grey, red, green)
 let array = JSON.parse(localStorage.getItem('array'));
 $('input').each(function(index, element) {
     element.value = array[index];
+    //applies backgroudn color depending on time
+    if (parseInt(currentTime.format('H')) > index +9) {
+        element.style.backgroundColor = 'grey';
+    }
+    else if (parseInt(currentTime.format('H')) === index +9) {
+        element.style.backgroundColor = 'red';
+    }
+    else if (parseInt(currentTime.format('H')) < index +9) {
+        element.style.backgroundColor = 'lightgreen';
+    }
 });
+
