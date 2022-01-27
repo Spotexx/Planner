d5f9930ff9997bee8eda8a7286d7d14feb24be1f
Spotexx <travesimo@gmail.com>
2022-01-27 17:39:36 -0500

    removed debugging console.logs

--- Assets/index.js
+++ Assets/index.js
@@ -2,29 +2,27 @@ let currentTime = moment();
 $('#currentDay').text(currentTime.format('dddd, MMMM Do'));
 
 //stores array of notes to local storage
-$( "form" ).submit(function( event ) {
+$("form").submit(function (event) {
     event.preventDefault();
-    console.log('form submitted');
     let array = [];
-    $('input').each(function(index, element) {
+    $('input').each(function (index, element) {
         array.push(element.value);
     });
-    console.log(array);
     localStorage.setItem('array', JSON.stringify(array));
 });
 
 // loads arrays into text fields on load then feeds background color depending on current time(grey, red, green)
 let array = JSON.parse(localStorage.getItem('array'));
-$('input').each(function(index, element) {
+$('input').each(function (index, element) {
     element.value = array[index];
     //applies backgroudn color depending on time
-    if (parseInt(currentTime.format('H')) > index +9) {
-        element.style.backgroundColor = 'grey';
+    if (parseInt(currentTime.format('H')) > index + 9) {
+        element.style.backgroundColor = 'lightgrey';
     }
-    else if (parseInt(currentTime.format('H')) === index +9) {
-        element.style.backgroundColor = 'red';
+    else if (parseInt(currentTime.format('H')) === index + 9) {
+        element.style.backgroundColor = 'pink';
     }
-    else if (parseInt(currentTime.format('H')) < index +9) {
+    else if (parseInt(currentTime.format('H')) < index + 9) {
         element.style.backgroundColor = 'lightgreen';
     }
 });
