dda3f94be79ab8abe7cd0dd68940e9b35c59b5bf
Spotexx <travesimo@gmail.com>
2022-01-26 19:37:18 -0500

    created submit to store array of todo's to local storage

--- Assets/index.js
+++ Assets/index.js
@@ -3,3 +3,16 @@ $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
 //color code each time block based on past, present, or future grey, red, green
 //add event listener to each time block to add event to local storage
+
+//holds every text input value in an array
+
+$( "form" ).submit(function( event ) {
+    event.preventDefault();
+    console.log('form submitted');
+    let array = [];
+    $('input').each(function(index, element) {
+        array.push(element.value);
+    });
+    console.log(array);
+    localStorage.setItem('array', JSON.stringify(array));
+});
